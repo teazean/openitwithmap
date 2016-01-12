18b50bc969d6c192333c6c230b0d2c87bd09d695
teazean <zhanglei55@zhanglei-mac.local>
2016-01-12 21:18:30 +0800

    first add

---
+++ popup.js
@@ -0,0 +1,11 @@
+var _url = "http://event.map.baidu.com/service/qrcode?url=";
+window.addEventListener("load", function() {
+	chrome.tabs.query({
+		currentWindow: true,
+		active: true
+	}, function(tabs) {
+		var url = tabs[0].url;
+		document.querySelector("#qr").src = _url + encodeURIComponent(url);
+		document.querySelector("#mapqr").src = _url + encodeURIComponent('baidumap://map/cost_share?url=' + encodeURIComponent(url));
+	});
+});
